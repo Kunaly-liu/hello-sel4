4176ab7e0e2257c3168ec12ca85eae085ba90de0
Stephen Sherratt <stephen@sherra.tt>
2022-12-04 19:30:43 +1100

    Add deps to shell.nix

--- shell.nix
+++ shell.nix
@@ -2,8 +2,24 @@
   pkgs ? import <nixpkgs> {}
 }:
 with pkgs;
+let
+  pythonWithPackages = python3.withPackages (pkg: [
+    pkg.six
+    pkg.future
+    pkg.setuptools
+    pkg.jinja2
+    pkg.ply
+    pkg.protobuf
+  ]);
+in
 mkShell {
   buildInputs = [
+    git
     gitRepo
+    pythonWithPackages
+    ninja
+    cmake
+    libxml2
+    qemu
   ];
 }
